f5879096e709a7084ac9d2b38ebfc194463c1378
Pavel_143477 <darthpsh@gmail.com>
2021-04-12 01:05:09 +0300

    Init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,96 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+
+  <el-row>
+    <el-input 
+      @input="inputWordsCount"
+      @keypress.enter="sortInputValues"
+      v-model="input"
+      type="text"
+      placeholder="Please input"
+    ></el-input>
+  </el-row>
+  
+  <el-row>
+    <el-button 
+    @click="sortInputValues"
+    type="primary"
+    >Сортировать</el-button>
+  </el-row>
+
+  <template 
+  v-if="this.inputArr.length">
+    <el-row>
+      <el-select v-model="value" :placeholder="value">
+        <el-option
+          v-for="item in sortInputValues()" 
+          :key="item"
+          :label="toLocaleUpperCase(item)"
+          :value="item"
+        ></el-option>
+      </el-select>
+    </el-row>
+  </template>
+
+
+  <div>
+    <el-checkbox-group v-model="checkboxGroup">
+      <el-checkbox-button
+      v-for="num in numbers" 
+      :label="num" 
+      :key="num"
+      >{{num}}</el-checkbox-button>
+    </el-checkbox-group>
+    <div>Сумма чекбоксов = {{ checkRes }}</div>
+  </div>
+  
+
 </template>
 
-<script>
-import HelloWorld from "./components/HelloWorld.vue";
 
-export default {
-  name: "App",
-  components: {
-    HelloWorld,
+
+<script>
+import { defineComponent, ref } from 'vue'
+export default defineComponent ({
+  setup() {
+    return {
+      input: ref('')
+    };
   },
-};
+  data() {
+    return {
+      inputArr: [],
+      value: 'Select',
+      checkboxGroup: [],
+      numbers: [5, 8, 12, 15, 22],
+    };
+  }, // data
+  methods: { 
+    inputWordsCount() {
+      
+    },
+    sortInputValues() {
+      this.inputArr = this.input;
+      return this.inputArr
+        .split(",")
+        .map(x => { return x.trim(); })
+        .sort(function (a, b) {
+          return a.localeCompare(b);
+        })
+        .filter(element => element !== "")
+    },
+    toLocaleUpperCase(item) {
+      return item.toLocaleUpperCase();
+    },
+  }, // methods
+  computed: {
+    checkRes() {
+    	return this.checkboxGroup.reduce(function (a, b) {
+      	return parseInt(a) + parseInt(b);
+      }, 0);
+    },
+  } // computed
+})
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
